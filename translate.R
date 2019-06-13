53f53072f446bc5aefb4bd13221feffae7051a38
Valeria <vvillaloboschiara@gmail.com>
2019-06-13 17:55:48 +0200

    adding utf8 encoding

--- translate.R
+++ translate.R
@@ -1,18 +1,18 @@
 library(dplyr)
 library(rvest)
+library(stringr)
 
 translate <- function(url){
-  site <- read_html(url)
+  site <- read_html(iconv(url, to = "UTF-8"), encoding = "utf8")
   
   from_word <- site %>%
     html_nodes('#articleWRD table.WRD:first-of-type .even .FrWrd strong') %>%
     html_text()
   
-  to <- site %>% 
+  to_word <- site %>% 
     html_nodes('#articleWRD table.WRD:first-of-type .even .ToWrd') %>%
+    html_nodes(xpath = 'text()') %>%
     html_text()
-  
-  to_word <- strsplit(to, "\\w+:.*") %>% str_trim()
   
   assign("to_word", to_word,  envir = .GlobalEnv)
   assign("from_word", from_word,  envir = .GlobalEnv)
