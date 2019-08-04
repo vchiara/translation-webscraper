71aa471e231a8a15fc365c25c1cd9807258df77d
Valeria <vvillaloboschiara@gmail.com>
2019-08-04 14:49:58 +0200

    Return translation list

--- wordreference_scraper.R
+++ wordreference_scraper.R
@@ -1,53 +1,52 @@
-library(dplyr)
+library(tidyverse)
 library(rvest)
 library(stringr)
-library(purrr)
+
+
+get_words <- function(nodes, class){
+ 
+  names(nodes) <- nodes %>% html_attr("class")
+  type <- names(nodes)==class
+  
+  word_list <- list()
+  current_list <- list()
+
+  i = 1
+  while(i <= length(nodes)){
+    
+    if(type[i]){
+      from <- nodes[i] %>% html_nodes('.FrWrd strong') %>% 
+              html_text() %>% 
+              str_remove_all("\u21D2")
+     
+       to <- nodes[i] %>% html_nodes('.ToWrd') %>% 
+              html_nodes(xpath = 'text()') %>% 
+              html_text() %>%
+              str_remove_all("\u21D2")
+      
+      current_list$from <- c(current_list$from, from) 
+      current_list$to <- c(current_list$to, to)
+      
+      if(i == length(nodes)){
+        word_list[[length(word_list)+1]] <- current_list
+      }
+      
+    } else if (length(current_list) > 0) {
+      word_list[[length(word_list)+1]] <- current_list
+      current_list <- list()
+    }
+    
+    i = i + 1
+  }
+  
+  return(word_list)
+}
+
 
 translate <- function(url){
+  
   site <- read_html(iconv(url, to = "UTF-8"), encoding = "utf8")
-  
-  from_word <- site %>%
-    html_nodes('#articleWRD table.WRD:first-of-type .even .FrWrd strong, 
-               #article table.WRD:first-of-type .even .FrWrd strong') %>%
-    html_text() %>%
-    str_remove_all("\u21D2")
-  
-  to_word <- site %>% 
-    html_nodes('#articleWRD table.WRD:first-of-type .even .ToWrd, 
-               #article table.WRD:first-of-type .even .ToWrd') %>%
-    html_nodes(xpath = 'text()') %>%
-    html_text() %>%
-    str_remove_all("\u21D2")
-  
-  if (is_empty(c(from_word, to_word))) {
-    script <- site %>%
-      html_nodes("#articleHead > script") %>% 
-      html_attr("src")
-    
-    suggestions <- read_html(script) %>%
-      html_nodes("a") %>% 
-      html_text()
-    
-    if (is_empty(suggestions)) {
-      assign("suggestions", '', envir = .GlobalEnv)
-    } else {
-      assign("suggestions", suggestions, envir = .GlobalEnv)
-    }
-  }
-  
-  if (length(to_word) > 1) {
-    assign("to_word", sapply(to_word, URLencode),  envir = .GlobalEnv)
-  } else {
-    global_to_word <- if ( length(to_word) == 1) URLencode(to_word) else ''
-    assign("to_word", global_to_word,  envir = .GlobalEnv)
-  }
-  
-  if (length(from_word) > 1) {
-    assign("from_word", sapply(from_word, URLencode),  envir = .GlobalEnv)
-  } else {
-    global_from_word <- if ( length(from_word) == 1) URLencode(from_word) else ''
-    assign("from_word", global_from_word,  envir = .GlobalEnv)
-  }
+  nodes <- site %>% html_nodes('table.WRD > tr')
   
   language_source <- site %>%
     html_node(xpath = '/html/head/meta[3]') %>%
@@ -65,4 +64,22 @@ translate <- function(url){
   assign("language_source", language_source, envir = .GlobalEnv)
   assign("language_translation", language_translation, envir = .GlobalEnv)
   
+  if(is_empty(nodes)){
+    script <- site %>%
+      html_nodes("#articleHead > script") %>% 
+      html_attr("src")
+    
+    suggestions <- read_html(script) %>%
+      html_nodes("a") %>% 
+      html_text()
+    
+    assign("suggestions", suggestions, envir = .GlobalEnv)
+  }
+  
+  word_list_even <- get_words(nodes, "even")
+  word_list_odd <- get_words(nodes, "odd")
+  
+  translation <- append(word_list_even, word_list_odd)
+  
+  assign("translation", translation, envir = .GlobalEnv)
 }
